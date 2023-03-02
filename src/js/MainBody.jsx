3f5c0797236a95f9385953425892e290ade44d90
Jonas Perusquia <jonaspm_99@outlook.com>
2023-03-01 22:00:20 -0700

    chore/text fixes

--- src/js/MainBody.jsx
+++ src/js/MainBody.jsx
@@ -12,9 +12,9 @@ class MainBody extends React.Component {
                     I am passionate about technology, especially software, I love discovering new things, innovative tools and methods. I've been a scout since I was a kid, I like to be helpful, spend quality time with my family, play saxophone, fish and play video games. I enjoy playing sports and going camping.
                     I like to be a leader and work in a team, I am sociable, patient and like to make new friends.
                 </div>
-                <h1>Expertise</h1>
+                <h1>Background</h1>
                 <div className="flex flex-column align-center">
-                    <span>5+ years of experience working with multinational clients, improving the digital experience of business and individuals.</span>
+                    <span>5+ years of expertise working with small and multinational clients, creating and improving the digital experience of businesses and individuals.</span>
                     <h2>Technologies</h2>
                     <ul className="flex flex-row list">
                         <li>HTML</li>
