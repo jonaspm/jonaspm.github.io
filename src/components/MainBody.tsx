66be4e095fb4f38a4e12ccf05b1d26706d72551b
Jonas Perusquia <jperusm@outlook.com>
2023-04-26 12:56:29 -0600

    fix/tweaks to self-description

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -21,18 +21,21 @@ function MainBody() {
         'Next.js'
     ]
 
+    const aboutText = 'Software Engineer whose passion for technology and web development drives him to constantly learn and use the latest tools and trends in the industry. Has strong leadership, self-learning, problem-solving and perfectionism skills, as well as a positive, motivating and integrative attitude. He enjoys working in a team and sharing my knowledge and experiences with other professionals. In addition to technology, he has interests in music, sports, family, friends, movies, tv series and video games. Feel free to contact him if it is your desire to know more about his projects.'
+    const backgroundText = 'With over 5 years of work experience enhancing the digital experience of people through innovative & cutting-edge technology solutions. Has collaborated with various clients and delivered cloud-based technology projects, including web, mobile and desktop applications.'
+
     return (
         <div className='mainBody max-w-7xl'>
             <Box className='body flex flex-col mt-6 items-center shadow-lg'>
                 <h2 className='text-xl font-bold'>About me</h2>
                 <div className="text-justify m-4">
-                    I am passionate about technology and web development, always looking to learn and use the latest tools and trends in the industry. I consider myself a leader, a self-taught, a perfectionist and a problem solver, with a positive, motivating and inclusive attitude. I like to work in a team and share my knowledge and experiences with other professionals. Besides technology, my hobbies are music, sports, family, friends, movies, series and video games. If you want to know more about me or my projects, do not hesitate to contact me.
+                    {aboutText}
                 </div>
             </Box>
             <Box className='body flex flex-col items-center shadow-lg'>
                 <h2 className='text-xl font-bold'>Background</h2>
                 <div className="flex flex-col align-center m-4 text-justify">
-                        With more than 5 years of work experience improving the digital experience of people through innovative & edge tecnology solutions. Has working with various clients and technological cloud-based projects, ranging from websites to mobile and desktop applications.
+                    {backgroundText}
                 </div>
             </Box>
             <Box className='body flex flex-col mb-6 items-center shadow-lg'>
