b3c0258229c931e18a80b37b9bbcb54d115cab32
Jonas Perusquia Morales <jperusm@outlook.com>
2023-12-15 18:36:38 -0600

    fix/astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://jonasperusquia.com",
+  site: "https://jonaspm.github.io",
   integrations: [react(), tailwind()],
   server: {
     host: true,
