16a9bca9d36b073f18acf8a362fa7050bb616025
Jonas Perusquia Morales <jperusm@outlook.com>
2024-04-05 15:44:45 -0600

    tweaks to animations and css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://jonaspm.github.io",
   integrations: [react(), tailwind()],
   server: {
-    host: true,
-  },
+    host: true
+  }
 });
--- src/components/HeaderProfile.tsx
+++ src/components/HeaderProfile.tsx
@@ -5,7 +5,7 @@ function HeaderProfile() {
   return (
     <Box className="header-profile !backdrop-blur-none p-5 !bg-secondary/0">
       <Box className="aspect-square !rounded-full max-w-xs self-center profile-container">
-        <div className="profile aspect-square w-full rounded-full"></div>
+        <div className="profile aspect-square w-full rounded-full" />
       </Box>
     </Box>
   );
